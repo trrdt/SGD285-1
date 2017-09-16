869a11392792c6fff7e879506966d6b24a36a1c4
Jonathon Huskey <jonathonchuskey@gmail.com>
2017-09-16 13:08:31 -0400

    Added quote for Jonathon Huskey

--- Main.cs
+++ Main.cs
@@ -32,20 +32,22 @@ public class SGD285_1
     // Main method
     public static void Main()
     {
-        UserQuote[] userQuotes = new UserQuote[] {
-            new UserQuote("Bahram Dahi", "I have never let my schooling interfere with my education. - [attributed to] Mark Twain"),
-			new UserQuote("Ryan Douglass", "If there is no struggle there is no progress. - Frederick Douglass"),
-				new UserQuote("Baylen Smith", "Whatever happens, happens. - Spike Spiegel"),
-					new UserQuote("Jennifer Ethridge", "I have found a desire within myself that no experience in this world can satisfy; the most probable explanation is that I was made for another world."),
-                    new UserQuote("Haley Sessions", "No road is long with good company. - Turkish Proverb"),
-                    new UserQuote("Paola Gonzalez", "The roots of education are bitter, but the fruit is sweet. - Aristotle"),
-                    new UserQuote("Dylan Zuniga", "All the love as always. H - Harry Styles"),
-                        new UserQuote("Kendall Wix", "At the end of this day, one shall stand, one shall fall!. O - Optimus Prime"),	
-                        new UserQuote("Joseph Mitchell", "Education is a weapon, whose effect depends on who holds it in his hands and whom it is aimed. - Joseph Stalin"),					
-            new UserQuote("Preston Rockholt", "I am the wisest man alive, for I know one thing, and that is that I know nothing. - [attributed to] Socrates")
-            // add the next user here using the formula above, don't forget the comma to separate the items in the initialization list. 
-            // IMPORTANT: If there is a conflict, you MUST fix it, compile to make sure 
-            // your program compiles and runs locally, then push to the git repository on GitHub.
+        UserQuote[] userQuotes = new UserQuote[] 
+	    {
+           new UserQuote("Bahram Dahi", "I have never let my schooling interfere with my education. - [attributed to] Mark Twain"),
+	       new UserQuote("Ryan Douglass", "If there is no struggle there is no progress. - Frederick Douglass"),
+	       new UserQuote("Baylen Smith", "Whatever happens, happens. - Spike Spiegel"),
+	       new UserQuote("Jennifer Ethridge", "I have found a desire within myself that no experience in this world can satisfy; the most probable explanation is that I was made for another world."),
+           new UserQuote("Haley Sessions", "No road is long with good company. - Turkish Proverb"),
+           new UserQuote("Paola Gonzalez", "The roots of education are bitter, but the fruit is sweet. - Aristotle"),
+           new UserQuote("Dylan Zuniga", "All the love as always. H - Harry Styles"),
+           new UserQuote("Kendall Wix", "At the end of this day, one shall stand, one shall fall!. O - Optimus Prime"),	
+           new UserQuote("Joseph Mitchell", "Education is a weapon, whose effect depends on who holds it in his hands and whom it is aimed. - Joseph Stalin"),					
+           new UserQuote("Preston Rockholt", "I am the wisest man alive, for I know one thing, and that is that I know nothing. - [attributed to] Socrates"),
+	       new UserQuote("Jonathon Huskey", "The path of least resistance is the path of the loser. -H. G. Wells")
+           // add the next user here using the formula above, don't forget the comma to separate the items in the initialization list. 
+           // IMPORTANT: If there is a conflict, you MUST fix it, compile to make sure 
+           // your program compiles and runs locally, then push to the git repository on GitHub.
         };
 
         // Do not modify below this line
