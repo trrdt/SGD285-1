1d7ac3fd2fe2d7b05975d3a5f5007e1cc79418d6
AdamSeymour <67116242+AdamSeymour@users.noreply.github.com>
2020-06-26 12:47:59 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -44,7 +44,7 @@ public class SGD285_1
            new UserQuote("Kendall Wix", "At the end of this day, one shall stand, one shall fall!. O - Optimus Prime"),
            new UserQuote("Joseph Mitchell", "Education is a weapon, whose effect depends on who holds it in his hands and whom it is aimed. - Joseph Stalin"),
            new UserQuote("Preston Rockholt", "I am the wisest man alive, for I know one thing, and that is that I know nothing. - [attributed to] Socrates"),
-	       new UserQuote("Jonathon Huskey", "The path of least resistance is the path of the loser. -H. G. Wells"),
+	       new UserQuote("Jonathon Huskey", "The path of least resistance is the path of the loser. -H. G. Wells"), /* A good quote about how you must work hard to acheve things in life - Adam Seymour*/
            new UserQuote("Jennifer Baldwin", "I am no bird; and no net ensnares me; I am a free human being with an independent will. - Charlotte Bronte"),
 	       new UserQuote("Jonny Westfall", "No one will ever get what they want, and that is beautiful. - They Might Be Giants"),
            new UserQuote("Joseph Flanagan", "'Do. Or do not. There is no try.' -Yoda"), // I love this quote//
@@ -54,14 +54,15 @@ public class SGD285_1
            new UserQuote("Hannah Sprinkle", "There are no happy endings, endings are the saddest part. So just give me a happy middle and a very happy start. -Shel Silverstein"),
            new UserQuote("Evy Kollstrand", "You never know until you know, you know"),
            new UserQuote("Keivon Arooji", "Education is what remains after one has forgotten what one has learned in school. -Albert Einstein"),
-           new UserQuote("Darius Samani", "If I had nine hours to chop down a tree, Iíd spend the first six sharpening my axe.î ñ Abraham Lincoln"),
+           new UserQuote("Darius Samani", "If I had nine hours to chop down a tree, I‚Äôd spend the first six sharpening my axe.‚Äù ‚Äì Abraham Lincoln"),
            new UserQuote("Samuel Hinkelman", "If you never stop learning you will never stop seeing the possibilities. -Bill Gates"), /* Brilliant quote! - Aliyah Day */
            new UserQuote("Tobias Gladden", "I? I am a monument to all your sins. - The Gravemind"),
            new UserQuote("Samuel Hinkelman", "If you never stop learning you will never stop seeing the possibilities. -Bill Gates"),
            new UserQuote("Aliyah Day", "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty. -Maya Angelou"),
-           new UserQuote("Perry Courter", "Hell of a thing killing a man, you take away all he has, and everything he is ever gunna be - Clint Eastwood" ),
+           new UserQuote("Perry Courter", "Hell of a thing killing a man, you take away all he has, and everything he is ever gunna be - Clint Eastwood" ), /* A clasic quote from a clasic man - Adam Seymour */
            new UserQuote("Thomas Robertson", "I come to trade and to learn. - Barnabas Sackett"),
            new UserQuote("This is a Test", "Something witty is supposed to go here = Me")
+	   new UderQuote("Adam Seymour", "Lift itself is only a vision, a dream. nothing exists except empty space, and you. And you are but a thought - Mark Twain Mysterious stranger"),
            // add the next user here using the formula above, don't forget the comma to separate the items in the initialization list.
            // IMPORTANT: If there is a conflict, you MUST fix it, compile to make sure
            // your program compiles and runs locally, then push to the git repository on GitHub.
